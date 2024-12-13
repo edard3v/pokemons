23c1bdec7f2d29428dc4b2eaa490ad2b4df6824e
edard3v <edard3v@gmail.com>
2024-12-13 03:23:02 -0500

    primer fetch server only

---
+++ src/pages/router.ts
@@ -0,0 +1,6 @@
+export const ROUTER = {
+  index: {
+    display: "Inicio",
+    path: "/",
+  },
+};
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface PokemonsRes {
+  count: number;
+  next: string;
+  previous: null;
+  results: Result[];
+}
+
+export interface Result {
+  name: string;
+  url: string;
+}

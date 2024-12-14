ebde180324dfd483aa06069cce340cdc0b9691ad
edard3v <edard3v@gmail.com>
2024-12-14 16:36:20 -0500

    agregando seo

---
+++ src/consts/seo.ts
@@ -0,0 +1,9 @@
+export const SEO = {
+  title: "Pokédex Virtual - Encuentra tus Pokémon Favoritos",
+  author: "edard3v",
+  description:
+    "Explora la Pokédex más completa con datos sobre tus Pokémon favoritos. Descubre estadísticas, evoluciones y habilidades en un solo lugar.",
+  img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  keywords:
+    "Pokédex, Pokémon, datos de Pokémon, habilidades Pokémon, evoluciones Pokémon, estadísticas de Pokémon, guía Pokémon, Pikachu, Charizard, juegos de Pokémon, Pokémon tipos, PokeAPI, edard3v",
+};

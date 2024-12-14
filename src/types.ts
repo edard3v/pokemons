f5f4ed30af32988224bcc3005d6012ed9675fd58
edard3v <edard3v@gmail.com>
2024-12-14 01:53:53 -0500

    iniciando paginación estatica

--- src/types.ts
+++ src/types.ts
@@ -2,10 +2,10 @@ export interface PokemonsRes {
   count: number;
   next: string;
   previous: null;
-  results: Result[];
+  results: Pokemon[];
 }
 
-export interface Result {
+export interface Pokemon {
   name: string;
   url: string;
 }

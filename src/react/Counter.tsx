a53a5a4880f147d93a19a8b594b78f612e392300
edard3v <edard3v@gmail.com>
2024-12-16 02:55:39 -0500

    refactor islas

--- src/react/Counter.tsx
+++ src/react/Counter.tsx
@@ -4,13 +4,21 @@ export default function Counter() {
   const [counter, setCounter] = useState(0);
   return (
     <div>
-      <div>{counter}</div>
-      <button className="border p-2" onClick={() => setCounter(counter + 1)}>
-        +1
-      </button>
-      <button className="border p-2" onClick={() => setCounter(counter - 1)}>
-        -1
-      </button>
+      <div className="text-center text-2xl">{counter}</div>
+      <div className="flex gap-2">
+        <button
+          className="border-2 border-black px-2 rounded"
+          onClick={() => setCounter(counter + 1)}
+        >
+          +1
+        </button>
+        <button
+          className="border-2 border-black px-2 rounded"
+          onClick={() => setCounter(counter - 1)}
+        >
+          -1
+        </button>
+      </div>
     </div>
   );
 }
